d6a7fdd31149d2d60aeaa42ebc7098e4e257edc4
Zwz-rookie <76395561+Zwz-rookie@users.noreply.github.com>
2021-01-27 17:11:37 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+
+int main(void)
+{
+  int a=10;
+  int b=20;
+  int c=0;
+  c = a+b;
+  cout<<"Hello World!"<<endl;
+  cout<<"a+b="<<a<<"+"<<b<<"="<<c<<endl;
+  return 0;
+}
